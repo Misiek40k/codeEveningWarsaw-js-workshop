e799d74068d1a308eece47a8fd69f6259c7ef497
Misiek40k <comanderrr@gmail.com>
2019-02-21 19:44:52 +0100

    popup.js show and hide function added

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+const showPopup = (event) => {
+    event.preventDefault();
+    document.getElementById('popup').style.display = 'block';
+}
+
+const hidePopup = (event) => {
+    event.preventDefault();
+    document.getElementById('popup').style.display = 'none';
+}
